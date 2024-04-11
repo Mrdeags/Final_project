4d8c4ce7c681d6b82d5b53c1dd04bcde1044bd6e
Mrdeags <tylertodd2011@yahoo.com>
2024-04-11 12:47:46 -0500

    Fixed image source upload

--- upload-image.php
+++ upload-image.php
@@ -5,8 +5,8 @@ if(isset($_POST["image"])){
     $image_array_2 = explode(",", $image_array_1[1]);//Split "base64" with "abc123••••"
     $data = base64_decode($image_array_2[1]);//Decodes base_64 encoded string back to image data
     $randomNumber = mt_rand(10000000, 99999999); //Generate random number for image name
-    $imageName = "/FINAL/assets/images/".$randomNumber.'.png'; //setting image location (For img src for displaying image on the page), this can be relative
-    $directory = $_SERVER['DOCUMENT_ROOT']."/FINAL/assets/images/".$randomNumber. '.png'; //Setting absolute directory on the server with
+    $imageName = "/cweb/FINAL/assets/images/".$randomNumber.'.png'; //setting image location (For img src for displaying image on the page), this can be relative
+    $directory = $_SERVER['DOCUMENT_ROOT']."/cweb/FINAL/assets/images/".$randomNumber. '.png'; //Setting absolute directory on the server with
     //SERVER DOCUMENT ROOT (To make sure file directory stays the same between servers) for storing file to server
     file_put_contents($directory, $data); //Write data to file on the server
     echo '<input type="hidden" value="' . $randomNumber . '" id="eqImg"/>'; //Create hidden input with the image identifier
